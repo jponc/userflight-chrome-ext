afd527d367d929287f4f46dab870dd8747910489
Julian Alfonso Ponce <ponce.julianalfonso@gmail.com>
2020-05-24 21:28:14 +1000

    Set CRA homepage to ./ to fix build step

--- src/popup/src/App.tsx
+++ src/popup/src/App.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [currentUrl, setCurrentUrl] = useState<string>("")
+
+  chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+    setCurrentUrl(tabs[0].url || "");
+  })
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Current url: {currentUrl}
         </p>
         <a
           className="App-link"
