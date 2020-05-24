a8356e96df732994c57e7102185031e0c8742d8b
Julian Alfonso Ponce <ponce.julianalfonso@gmail.com>
2020-05-24 19:38:12 +1000

    Output contentscript files to dist so chrome can import everything in dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,26 @@
-const { CheckerPlugin } = require("awesome-typescript-loader");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const { optimize } = require("webpack");
-const { join } = require("path");
+const { CheckerPlugin } = require('awesome-typescript-loader');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { optimize } = require('webpack');
+const { join } = require('path');
 let prodPlugins = [];
-if (process.env.NODE_ENV === "production") {
+
+if (process.env.NODE_ENV === 'production') {
   prodPlugins.push(
     new optimize.AggressiveMergingPlugin(),
     new optimize.OccurrenceOrderPlugin()
   );
 }
+
 module.exports = {
   mode: process.env.NODE_ENV,
-  devtool: "inline-source-map",
+  devtool: 'inline-source-map',
   entry: {
-    contentscript: join(__dirname, "src/contentscript/contentscript.ts"),
-    background: join(__dirname, "src/background/background.ts"),
+    contentscript: join(__dirname, 'src/contentscript/contentscript.ts'),
+    background: join(__dirname, 'src/background/background.ts'),
   },
   output: {
-    path: join(__dirname, "dist"),
-    filename: "[name].js",
+    path: join(__dirname, 'dist'),
+    filename: '[name].js',
   },
   module: {
     rules: [
@@ -29,7 +31,7 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
     ],
   },
@@ -37,11 +39,11 @@ module.exports = {
     new CheckerPlugin(),
     ...prodPlugins,
     new MiniCssExtractPlugin({
-      filename: "[name].css",
-      chunkFilename: "[id].css",
+      filename: '[name].css',
+      chunkFilename: '[id].css',
     }),
   ],
   resolve: {
-    extensions: [".ts", ".js"],
+    extensions: ['.ts', '.js'],
   },
 };
